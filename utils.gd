77444c4938a48811e56b65eb6699cd4cb6851206
jcomcl <j@jcom.cl>
2026-05-30 15:17:57 +0100

    very broken movement code

--- utils.gd
+++ utils.gd
@@ -1,12 +1,34 @@
 extends Node
 class_name Utils
 
-func move(n:CollisionObject3D, dir:Vector3):
-	var collision = n.move_and_collide(dir)
+func move(n: CollisionObject3D, dir: Vector3):
+	var grid = _get_grid(n)
+	var start_pos = n.global_position
+	var target_pos: Vector3
+	if grid:
+		target_pos = grid.snapped_to_grid(start_pos + dir)
+	else:
+		target_pos = start_pos + dir
+	var move_vec = Vector3(target_pos.x - start_pos.x, 0, target_pos.z - start_pos.z)
+
+	var collision = n.move_and_collide(move_vec)
+
 	if collision:
-		var collided_object = collision.get_collider()
-		if collided_object.has_method("nudge"):
-			collided_object.nudge(dir)
+		var collider = collision.get_collider()
+		var nudge_success = collider.has_method("nudge") and collider.nudge(dir)
+		if nudge_success:
+			n.global_position.x = target_pos.x
+			n.global_position.z = target_pos.z
+		else:
+			n.global_position = start_pos
+			return
+	else:
+		n.global_position.x = target_pos.x
+		n.global_position.z = target_pos.z
+
+	if grid:
+		var pos = grid.get_object_grid_pos(n)
+		grid.set_at_pos(n, pos.x, pos.y)
 
 func register(n: Node3D):
 	var grid = _get_grid(n)
