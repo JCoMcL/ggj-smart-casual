003d3a34df039a478d6aefaf8f8a9b292f0ca5c1
jcomcl <j@jcom.cl>
2026-05-31 10:53:31 +0100

    make tile entities clickable

--- Entities/tile_entity.gd
+++ Entities/tile_entity.gd
@@ -3,6 +3,7 @@ class_name TileEntity
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	input_ray_pickable = true
 	grid.snap_to_grid(self)
 	grid.update_pos(self)
 	grid.step.connect(grid_step)
@@ -13,3 +14,14 @@ func grid_step() -> void:
 
 func grid_update() -> void:
 	pass
+
+func _on_click():
+	print(self, "clicked")
+
+func _input_event(camera: Camera3D, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if (
+			event is InputEventMouseButton and
+			event.pressed and
+			event.button_index == MOUSE_BUTTON_LEFT
+	):
+		_on_click()
