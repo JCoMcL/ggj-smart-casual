6e46bd5773bdb14b5fe8feef19b63851e1b71515
FrankFrank5678 <mprionnsias@gmail.com>
2026-05-31 14:50:35 +0100

    ui update

---
+++ UI_scripts/start_game_Level_4_button.gd
@@ -0,0 +1,10 @@
+extends BaseButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pressed.connect(_on_pressed)
+	pass # Replace with function body.
+
+func _on_pressed():
+	get_tree().paused = false
+	Root.get_root(self).change_scene(3)
